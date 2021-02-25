d321396e39645dbfae1efffbb6fd0e6ea4983f3d
Deyvid William <deyvidwms@gmail.com>
2021-02-25 09:24:45 -0300

    "remove variable"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 
 import Login from './pages/Login';
-import Perfil from './pages/Perfil';
 
 function App() {
   return <Login/>;
-  // return <Perfil />;
 }
 
 export default App;
