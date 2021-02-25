b6c7283f4ce5c36761a149920c36eccd477597f5
Deyvid William <deyvidwms@gmail.com>
2021-02-25 08:46:08 -0300

    recovery project

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Login from './pages/Login';
 import Perfil from './pages/Perfil';
 
 function App() {
-  // return <Login/>;
-  return <Perfil />;
+  return <Login/>;
+  // return <Perfil />;
 }
 
 export default App;
