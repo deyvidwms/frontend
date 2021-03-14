0d43937b8b436be2c4aaabf40f3aa48bfbf679c6
Deyvid William <deyvidwms@gmail.com>
2021-03-14 20:23:45 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,8 @@ const api = axios.create({
   // baseURL: "http://127.0.0.1:8000",
   headers: {
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
   },
 });
 
